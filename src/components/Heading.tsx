1001836b95596392aa9dc57cb8a5e76ef88dda24
michaelclemens <michaelclemens8@hotmail.com>
2025-03-03 20:48:51 +0000

    Fixing report issue

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,12 +6,12 @@ export default function Heading() {
 
   return (
     <header className="ring-header-outline mb-10 flex w-full items-center rounded-sm py-4 pr-4 pl-7 ring-3 lg:w-5xl">
-      <h1 className="flex-grow">
-        <ul className={`${isBonusGame ? 'text-lg leading-4' : 'text-4xl leading-7'} text-barlow-semi text-white uppercase`}>
-          {availableSelections.map(selection => (
-            <li key={selection}>{selection}</li>
-          ))}
-        </ul>
+      <h1 className={`${isBonusGame ? 'text-lg leading-4' : 'text-4xl leading-7'} text-barlow-semi flex-grow text-white uppercase`}>
+        {availableSelections.map(selection => (
+          <span key={selection} className="block">
+            {selection}
+          </span>
+        ))}
       </h1>
       <Score />
     </header>
